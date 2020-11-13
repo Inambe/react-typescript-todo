2949666bd8f74732597903dd3044f5f86343b8e6
Inam Ul Huq <inambe.dev@gmail.com>
2020-11-13 17:33:55 +0500

    first look

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Todo from "../../models/todo";
 import "./App.css";
 
 function App() {
+	const [todos, setTodos] = useState<Todo[]>([]);
+	const [todo, setTodo] = useState<string>("");
+
+	const onSaveClick = () => {
+		if (todo) {
+			const newTodo = new Todo(todo, todos.length);
+			setTodos([...todos, newTodo]);
+			setTodo("");
+		}
+	};
+
 	return (
-		<div>
-			<header>Header</header>
-			<p>Hello G.</p>
+		<div className="container">
+			<header>Todoz</header>
+			<input
+				onChange={(e) => {
+					setTodo(e.target.value);
+				}}
+				value={todo}
+			/>
+			<button onClick={onSaveClick}>Save</button>
+			<hr />
+			<ul>
+				{todos.map((todo) => {
+					return (
+						<li data-id={todo.id} key={todo.id}>
+							{todo.content}
+						</li>
+					);
+				})}
+			</ul>
 		</div>
 	);
 }
---
+++ src/models/todo.tsx
@@ -0,0 +1,9 @@
+class Todo {
+	id: number;
+	content: string;
+	constructor(content: string, id: number) {
+		this.content = content;
+		this.id = id;
+	}
+}
+export default Todo;
